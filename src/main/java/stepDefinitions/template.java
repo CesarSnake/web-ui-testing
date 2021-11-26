0c68150eeda32e557f7ee1a61e178c347623f083
Cesar_Snake <snakemgs14@gmail.com>
2021-11-26 20:49:02 +0100

    re-added setup chrome [Fix git acctions]

--- src/main/java/stepDefinitions/template.java
+++
@@ -1,6 +0,0 @@
-package stepDefinitions;
-
-public class template {
-    // void class to avoid the maven error about
-    // "Failed to execute goal org.apache.maven.plugins:maven-compiler-plugin:3.8.1:testCompile"
-}
