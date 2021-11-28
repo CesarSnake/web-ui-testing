d02e541cfa608fee1c9b6a8d375dc9879df95253
Cesar_Snake <snakemgs14@gmail.com>
2021-11-28 18:44:06 +0100

    added RadioButton step definitions [Fix Build]

---
+++ src/test/java/stepDefinitions/elements/RadioButtonStepDefinitions.java
@@ -0,0 +1,56 @@
+package stepDefinitions.elements;
+
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.*;
+import utils.TestUtils;
+
+public class RadioButtonStepDefinitions {
+    Scenario scenario;
+    WebDriver driver;
+
+    @Before
+    public void before(Scenario scenario) {
+        this.scenario = scenario;
+        driver = TestUtils.GetChromeDriver();
+    }
+
+    @Given("I go to the Radio Button webpage {string}")
+    public void i_go_to_the_radio_button_webpage(String webpage) {
+        driver.get(webpage);
+    }
+
+    @Then("I click the radio button {string}")
+    public void iClickTheRadioButton(String radioButtonId) {
+        driver.findElement(By.xpath("//*[contains(@for, '" + radioButtonId + "')]"))
+            .click();
+    }
+
+    @And("I check the radio button {string} is selected")
+    public void iCheckTheRadioButtonIsSelected(String radioButton) {
+        WebElement resultElement = driver.findElement(By.className("text-success"));
+        Assertions.assertEquals(radioButton, resultElement.getText());
+    }
+
+    @Then("I check the radio button {string} is disabled")
+    public void iCheckTheRadioButtonIsDisabled(String radioButtonId) {
+        WebElement radioButton = driver.findElement(By.id(radioButtonId));
+        String classAttribute = radioButton.getAttribute("class");
+        Assertions.assertTrue(classAttribute.contains("disabled"));
+    }
+
+    @And("I take a radio button screenshot with the name {string}")
+    public void iTakeARadioButtonScreenshotWithTheName(String fileName) {
+        byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+        scenario.attach(screenshot, "image/png", fileName);
+    }
+
+    @And("I close the Radio button webpage")
+    public void iCloseTheRadioButtonWebpage() {
+        driver.close();
+    }
+}
--- src/test/resources/features/elements/RadioButton.feature
+++ src/test/resources/features/elements/RadioButton.feature
@@ -2,11 +2,12 @@ Feature: Test radio button element of a webPage
 
   Scenario: Radio Button
     Given I go to the Radio Button webpage "https://demoqa.com/radio-button"
-    And I take a radio button page screenshot with the name "radioButtonStart"
+    And I take a radio button screenshot with the name "radioButtonStart"
     Then I click the radio button "yesRadio"
-    And I check the radio button "yesRadio" is selected
+    And I check the radio button "Yes" is selected
     And I take a radio button screenshot with the name "radioYes"
     Then I click the radio button "impressiveRadio"
-    And I check the radio button "impressiveRadio" is selected
+    And I check the radio button "Impressive" is selected
     And I take a radio button screenshot with the name "radioImpressive"
     Then I check the radio button "noRadio" is disabled
+    And I close the Radio button webpage
