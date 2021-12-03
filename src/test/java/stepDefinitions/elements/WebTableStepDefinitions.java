232ebb59cb3426dd848d54632574be6faa8abbe7
Cesar_Snake <snakemgs14@gmail.com>
2021-12-03 20:40:54 +0100

    refactor webTable

--- src/test/java/stepDefinitions/elements/WebTableStepDefinitions.java
+++ src/test/java/stepDefinitions/elements/WebTableStepDefinitions.java
@@ -1,7 +1,5 @@
 package stepDefinitions.elements;
 
-import io.cucumber.datatable.DataTable;
-import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
 import io.cucumber.java.en.And;
@@ -28,84 +26,108 @@ public class WebTableStepDefinitions {
         driver = TestUtils.GetChromeDriver();
     }
 
-    @After("@WebTable")
-    public void after() {
-        driver.quit();
-    }
-
-    @Given("I go to the Web Table webpage {string}")
-    public void iGoToTheWebTableWebpage(String webpage) {
-        driver.get(webpage);
+    @Given("I go to the WebTable webpage")
+    public void iGoToTheWebTableWebpage() {
+        driver.get("https://demoqa.com/webtables");
     }
 
     @Then("I check web table contains the elements:")
-    public void iCheckWebTableContainsTheElements(DataTable table) {
-        List<List<String>> rows = table.asLists(String.class);
+    public void iCheckWebTableContainsTheElements( List<List<String>> rowsExpected) {
+        // Get the webTable
+        WebElement tableElement = driver.findElement(
+            By.className("rt-tbody"));
 
-        WebElement tableElement = driver.findElement(By.className("rt-tbody"));
-        List<WebElement> tableRowElements = tableElement.findElements(By.className("rt-tr"));
+        // from the webTable get only the rows (header is not needed)
+        List<WebElement> tableRowElements = tableElement.findElements(
+            By.className("rt-tr"));
 
         for (int i = 0; i < tableRowElements.size(); i++) {
-            List<WebElement> cellsInRow = tableRowElements.get(i).findElements(By.className("rt-td"));
-            cellsInRow.remove(cellsInRow.size()-1); // the last element is the action buttons
+            // from each row, let's take each cell
+            List<WebElement> cellsInRowDisplayed = tableRowElements
+                .get(i)
+                .findElements(
+                    By.className("rt-td"));
 
-            for (int j = 0; j < cellsInRow.size(); j++) {
-                String cellValue = cellsInRow.get(j).getText();
+            cellsInRowDisplayed.remove(
+            cellsInRowDisplayed.size() - 1); // the last element is the action buttons
+
+            for (int j = 0; j < cellsInRowDisplayed.size(); j++) {
+                String cellValueDisplayed = cellsInRowDisplayed
+                    .get(j)
+                    .getText();
 
                 // As the table displays all the rows that the user wants, the empty rows are filled with "&nbsp;"
-                if (cellValue.equals(" ")) {
+                if (cellValueDisplayed.equals(" ")) {
                     continue;
                 }
 
-                assertEquals(rows.get(i).get(j), cellValue);
+                String cellValueExpected = rowsExpected
+                    .get(i)
+                    .get(j);
+
+                assertEquals(cellValueExpected, cellValueDisplayed);
             }
         }
     }
 
-    @Then("I check web table display {string} rows")
-    public void iCheckWebTableDisplayRows(String rowsNumber) {
-        WebElement tableElement = driver.findElement(By.className("rt-tbody"));
-        List<WebElement> tableRowElements = tableElement.findElements(By.className("rt-tr"));
+    @Then("I check web table displays {string} rows")
+    public void iCheckWebTableDisplaysRows(String rowsNumber) {
+        WebElement tableElement = driver.findElement(
+            By.className("rt-tbody"));
+
+        List<WebElement> tableRowElements = tableElement.findElements(
+            By.className("rt-tr"));
 
         assertEquals(Integer.valueOf(rowsNumber), tableRowElements.size());
     }
 
-    @When("I use the search Typing {string}")
+    @When("I use the search typing {string}")
     public void iUseTheSearchTyping(String typedSearch) {
-        WebElement searchElement = driver.findElement(By.id("searchBox"));
+        WebElement searchElement = driver.findElement(
+            By.id("searchBox"));
+
         searchElement.clear();
         searchElement.sendKeys(typedSearch);
     }
 
     @When("I press the web table button {string}")
     public void iPressTheWebTableButton(String buttonId) {
-        driver.findElement(By.id(buttonId))
+        driver.findElement(
+            By.id(buttonId))
             .click();
     }
 
     @Then("I check the registration form element {string} is {string}")
     public void iCheckTheRegistrationFormElementIs(String inputId, String value) {
-        String displayedValue = driver.findElement(By.id("userForm"))
-            .findElement(By.id(inputId)).getText();
+        String displayedValue = driver.findElement(
+            By.id("userForm"))
+            .findElement(
+                By.id(inputId))
+            .getText();
 
         assertEquals(value, displayedValue);
     }
 
     @Then("I check the registration form has not errors")
     public void iCheckTheRegistrationFormHasNotErrors() {
-        WebElement userFormElement = driver.findElement(By.id("userForm"));
+        WebElement userFormElement = driver.findElement(
+            By.id("userForm"));
+
         assertEquals("", userFormElement.getAttribute("class"));
     }
 
     @Then("I check the registration form has errors")
     public void iCheckTheRegistrationFormHasErrors() {
-        WebElement userFormElement = driver.findElement(By.id("userForm"));
+        WebElement userFormElement = driver.findElement(
+            By.id("userForm"));
+
         assertEquals("was-validated", userFormElement.getAttribute("class"));
     }
 
     @And("I fill the registration form typing on the element {string} the value {string}")
     public void iFillTheRegistrationFormTypingOnTheElementTheValue(String inputId, String value) {
-        WebElement inputElement = driver.findElement(By.id("userForm"))
+        WebElement inputElement = driver.findElement(
+            By.id("userForm"))
             .findElement(By.id(inputId));
 
         inputElement.clear();
@@ -114,23 +136,26 @@ public class WebTableStepDefinitions {
 
     @And("I check the edit registration form element {string} is {string}")
     public void iCheckTheEditRegistrationFormElementIs(String inputId, String value) {
-        String displayedValue = driver.findElement(By.id("userForm"))
+        String displayedValue = driver.findElement(
+            By.id("userForm"))
             .findElement(By.id(inputId))
             .getAttribute("value");
 
         assertEquals(value, displayedValue);
     }
 
-    @Then("I change the select to option {string} rows")
+    @When("I change the select to option {string} rows")
     public void iChangeTheSelectToOptionRows(String value) {
-        WebElement selectElement = driver.findElement(By.xpath("//select[contains(@aria-label, 'rows per page')]"));
+        WebElement selectElement = driver.findElement(
+            By.xpath("//select[@aria-label='rows per page']"));
+
         Select select = new Select(selectElement);
-        select.selectByValue(value) ;
+        select.selectByValue(value);
     }
 
-    @And("I close the Web Table webpage")
-    public void iCloseTheWebTableWebpage() {
-        driver.close();
+    @And("I quit the Web Table webpage")
+    public void iQuitTheWebTableWebpage() {
+        driver.quit();
     }
 
     @And("I take a web table page screenshot with fileName {string}")
--- src/test/resources/features/elements/WebTable.feature
+++ src/test/resources/features/elements/WebTable.feature
@@ -1,35 +1,35 @@
 @WebTable
-Feature: Test web table element of a webpage
+Feature: Test WebTable element of a webpage
 
-  Scenario: Checking on Web Table
-    Given I go to the Web Table webpage "https://demoqa.com/webtables"
+  Scenario: Searching on Web Table
+    Given I go to the WebTable webpage
     Then I check web table contains the elements:
       | Cierra | Vega     | 39 | cierra@example.com | 10000  | Insurance  |
       | Alden  | Cantrell | 45 | alden@example.com  | 12000  | Compliance |
       | Kierra | Gentry   | 29 | kierra@example.com | 2000   | Legal      |
-    And I check web table display "10" rows
+    And I check web table displays "10" rows
     And I take a web table page screenshot with fileName "webTableStart"
-    When I use the search Typing "Cierra"
+    When I use the search typing "Cierra"
     Then I check web table contains the elements:
       | Cierra | Vega     | 39 | cierra@example.com | 10000  | Insurance  |
     And I take a web table page screenshot with fileName "webTableSearchCierra"
-    When I use the search Typing "45"
+    When I use the search typing "45"
     Then I check web table contains the elements:
       | Alden  | Cantrell | 45 | alden@example.com  | 12000  | Compliance |
     And I take a web table page screenshot with fileName "webTableSearch45"
-    When I use the search Typing "legal"
+    When I use the search typing "legal"
     Then I check web table contains the elements:
       | Kierra | Gentry   | 29 | kierra@example.com | 2000   | Legal      |
     And I take a web table page screenshot with fileName "webTableSearchLegal"
-    When I use the search Typing "2000"
+    When I use the search typing "2000"
     Then I check web table contains the elements:
       | Alden  | Cantrell | 45 | alden@example.com  | 12000  | Compliance |
       | Kierra | Gentry   | 29 | kierra@example.com | 2000   | Legal      |
     And I take a web table page screenshot with fileName "webTableSearch2000"
-    And I close the Web Table webpage
+    And I quit the Web Table webpage
 
   Scenario: Adding Elements on Web Table
-    Given I go to the Web Table webpage "https://demoqa.com/webtables"
+    Given I go to the WebTable webpage
     When I press the web table button "addNewRecordButton"
     Then I check the registration form element "firstName" is ""
     Then I check the registration form element "lastName" is ""
@@ -56,10 +56,10 @@ Feature: Test web table element of a webpage
       | Kierra          | Gentry         | 29 | kierra@example.com | 2000  | Legal      |
       | First Name Test | Last Name Test | 31 | testing@test.com   | 3600  | TQS        |
     And I take a web table page screenshot with fileName "webTableAddedElement"
-    And I close the Web Table webpage
+    And I quit the Web Table webpage
 
   Scenario: Editing Elements on Web table
-    Given I go to the Web Table webpage "https://demoqa.com/webtables"
+    Given I go to the WebTable webpage
     Then I check web table contains the elements:
       | Cierra | Vega     | 39 | cierra@example.com | 10000 | Insurance  |
       | Alden  | Cantrell | 45 | alden@example.com  | 12000 | Compliance |
@@ -85,10 +85,10 @@ Feature: Test web table element of a webpage
       | Edited First Name | Edited Last Name | 99 | edited@test.com    | 9999  | edited    |
       | Kierra            | Gentry           | 29 | kierra@example.com | 2000  | Legal     |
     And I take a web table page screenshot with fileName "webTableEditedElement"
-    And I close the Web Table webpage
+    And I quit the Web Table webpage
 
-  Scenario: Deleting Elements Web table
-    Given I go to the Web Table webpage "https://demoqa.com/webtables"
+  Scenario: Deleting Elements on Web table
+    Given I go to the WebTable webpage
     Then I check web table contains the elements:
       | Cierra | Vega     | 39 | cierra@example.com | 10000 | Insurance  |
       | Alden  | Cantrell | 45 | alden@example.com  | 12000 | Compliance |
@@ -99,21 +99,21 @@ Feature: Test web table element of a webpage
       | Cierra | Vega     | 39 | cierra@example.com | 10000 | Insurance  |
       | Alden  | Cantrell | 45 | alden@example.com  | 12000 | Compliance |
     And I take a web table page screenshot with fileName "webTableDeleteEnd"
-    And I close the Web Table webpage
+    And I quit the Web Table webpage
 
   Scenario: Modifying rows size on Web table
-    Given I go to the Web Table webpage "https://demoqa.com/webtables"
-    Then I check web table display "10" rows
+    Given I go to the WebTable webpage
+    Then I check web table displays "10" rows
     When I change the select to option "5" rows
-    Then I check web table display "5" rows
+    Then I check web table displays "5" rows
     And I take a web table page screenshot with fileName "webTableSelectRow5"
     When I change the select to option "20" rows
-    Then I check web table display "20" rows
+    Then I check web table displays "20" rows
     And I take a web table page screenshot with fileName "webTableSelectRow20"
     When I change the select to option "25" rows
-    Then I check web table display "25" rows
+    Then I check web table displays "25" rows
     When I change the select to option "50" rows
-    Then I check web table display "50" rows
+    Then I check web table displays "50" rows
     When I change the select to option "100" rows
-    Then I check web table display "100" rows
-    And I close the Web Table webpage
+    Then I check web table displays "100" rows
+    And I quit the Web Table webpage
